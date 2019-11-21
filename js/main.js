d56bbe2b562ccea2bd02d2d01d0b91a24a3f7257
Kenny <kawhyte@gmail.com>
2019-11-21 14:43:15 -0800

    Formatted document

--- js/main.js
+++ js/main.js
@@ -1,34 +1,43 @@
-// DOM elements 
-const time  = document.getElementById('time');
-const greeting  = document.getElementById('greeting');
-const name  = document.getElementById('name');
-const focus  = document.getElementById('focus');
+// DOM elements
+const time = document.getElementById("time");
+const greeting = document.getElementById("greeting");
+const name = document.getElementById("name");
+const focus = document.getElementById("focus");
 
+// show time
+function showTime() {
+  let today = new Date();
+  //timey = today.getTime,
+  let hour = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
 
-// show time 
-function showTime(){
+  //set AM or PM
+  const amPm = hour >= 12 ? "PM" : "AM";
 
-    let today = new Date()
-    //timey = today.getTime,
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let sec = today.getSeconds();
+  // 12 Format
+  hour = hour % 12 || 12;
 
-    //set AM or PM
-    const amPm  = hour >= 12 ? 'PM' : 'AM';
+  //Output
+  time.innerHTML = `${hour}<span>:<span>${min}<span>:<span>${sec}`;
 
-    // 12 Format
-    hour = hour % 12 || 12
-
-    //Output
-     time.innerHTML =`${hour}<span>:<span>${min}<span>:<span>${sec}`;
- 
-
-    setTimeout(showTime,1000)
+  setTimeout(showTime, 1000);
 }
 
+//set background
+function setGreetings() {
+  let today = new Date();
+  let hour = today.getHours();
+
+  if (hour < 12) {
+    greeting.textContent = "Good morning";
+  } else if (hour < 18) {
+    greeting.textContent = "Good afternoon";
+  } else {
+    greeting.textContent = "Good evening";
+  }
+}
 
 //run
 let a = showTime();
-
-
+setGreetings();
